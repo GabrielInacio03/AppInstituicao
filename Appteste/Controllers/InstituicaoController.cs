314ea867abaff6a684207f047d83f3fb55257379
GabrielInacio03 <inaciogs03@gmail.com>
2022-03-28 09:44:27 -0300

    primeiro commit

---
+++ Appteste/Controllers/InstituicaoController.cs
@@ -0,0 +1,19 @@
+﻿using Appteste.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Appteste.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        public IActionResult Index()
+        {
+       
+            return View();
+        }
+        
+    }
+}
